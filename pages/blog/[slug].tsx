e49ace15c322cf8bdcb9ab8511aaa12596118b9f
dsab123 <dsabbaghumd@gmail.com>
2020-11-18 19:37:29 -0500

    attempt to prepend window.location.origin to og:image path

--- pages/blog/[slug].tsx
+++ pages/blog/[slug].tsx
@@ -1,14 +1,21 @@
 import Header from '../../components/Header'
 import Footer from '../../components/Footer'
 import Head from 'next/head';
+import React from 'react';
 
-export default function Blog(props) {
+export default function Blog() {
+    const [origin, setOrigin] = React.useState("");
+
+    React.useEffect(() => {
+      setOrigin(window.location.origin); // <-- here I get access to window
+    }, []);
+
     return <>
         <Head>
             <meta property="og:title" content="Blog Post | Daniel Sabbagh" key="title" />
             <meta property="og:description" content="This is a really cool blog post" key="description" />
             <meta property="og:type" content="article" key="type" />
-            <meta property="og:image" content="/silver.jpg" key="image" />
+            <meta property="og:image" content={`${origin}/silver.jpg`} key="image" />
         </Head>
             <Header></Header>
             <p>Header is up here?</p>
