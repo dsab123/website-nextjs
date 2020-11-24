a991cc519932919df2dc3a2869b6ad16344dd4d5
dsab123 <dsabbaghumd@gmail.com>
2020-11-24 01:53:15 -0500

    beginnings of header

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,9 +1,57 @@
+import Link from 'next/link';
 import styles from './Header.module.css'
+import { useEffect, useState } from 'react';
+import { useRouter } from 'next/router';
 
 export default function Header() {
-    return <div className="headerContent">
-        <div className="logoRow">
-        <a><div className="title"></div></a>
+    const toggleHamburger = () => {
+        console.log('toggled!');
+    }
+
+    const toggleHamburgerAndNavigate = () => {
+        console.log('toggleHamburgerAndNavigate!');
+    }
+
+    const hamburgerOpen = () => {
+         console.log('hamburgerOpen!');
+    }
+
+    const navigate = () => {
+        console.log('navigate!');
+    }
+
+    //const [hamburger, toggleHamburger] = useState(false);
+
+    const navItems = [
+        { id: 0, title: 'Home', uri: '/blog/4/about-me' },
+        { id: 1, title: 'About Me', uri: '/blog/4/about-me' },
+        { id: 2, title: 'Blog', uri: '/blogs' },
+        { id: 3, title: 'Book Summaries', uri: '/summaries' }
+    ];
+    
+    // todo get this working; do I need getStaticProps?
+    console.log('random element: ' + navItems[0].uri);
+
+    return <>
+        <div className={styles.headerContent}>
+            <div className={styles.logoRow}>
+                <Link href="http://localhost:3000">
+                    <a><div className={styles.title}></div></a>
+                </Link>
+                <div className={styles.hamburger} onClick={toggleHamburger}>
+                    <div className={styles.bar1}></div>
+                    <div className={styles.bar2}></div>
+                    <div className={styles.bar3}></div>
+                </div>
+            </div>
         </div>
-    </div>
+
+        <div className={styles.navigation}>
+            {navItems.map((item) => (
+                <Link key={item.id} href={item.uri}>
+                    <a className={styles.navItem}>{item.title}</a>
+                </Link>
+            ))}
+        </div> 
+    </>
 }
