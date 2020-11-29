53dfaa1d9807b496ad8ca3571310157702939a39
dsab123 <dsabbaghumd@gmail.com>
2020-11-29 06:45:23 -0500

    forgot one null check

--- pages/blog/[id]/[slug].tsx
+++ pages/blog/[id]/[slug].tsx
@@ -142,7 +142,7 @@ export default function Blog(props) {
 
                 
                 <div>
-                    {postInfo && postInfo.tags.length > 0 && 
+                    {postInfo.tags && postInfo.tags.length > 0 && 
                     <div className={styles.postTagContainer}>
                         <div>
                             <p className={styles.relatedPostsText}>related:</p>
@@ -183,7 +183,7 @@ export default function Blog(props) {
                             )}
                         </ul>}
                         
-                        {postInfo.tags.length == 0 && <p>Looks like there aren't any other posts with this tag 😔 <a href="mailto:dsabbaghumd@gmail.com" target="_blank">Want me to write one?</a></p>}
+                        {postInfo.tags && postInfo.tags.length == 0 && <p>Looks like there aren't any other posts with this tag 😔 <a href="mailto:dsabbaghumd@gmail.com" target="_blank">Want me to write one?</a></p>}
                     </div>
                 </div>
                 <br />
