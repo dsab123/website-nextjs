deb6e8de6190ef86e5bb79f2d5c6cbfdcb4ac9b2
dsab123 <dsabbaghumd@gmail.com>
2020-12-03 18:04:48 -0500

    freshen up og tags

--- pages/about.tsx
+++ pages/about.tsx
@@ -1,15 +1,18 @@
 import Head from 'next/head'
+import Link from 'next/link';
 import styles from '../styles/Home.module.css';
 
 export default function About() {
 
     return <>
         <Head>
-            <meta property="og:title" content="About Me | Daniel Sabbagh" key="title" />
-            <meta property="og:description" content="My goal is to help you read more books this year." key="description" />
+            <meta property="og:title" content="Why? | Daniel Sabbagh" key="title" />
+            <meta property="og:description" content="My goal is to help you read more this year." key="description" />
             <meta property="og:type" content="article" key="type" />
             <meta property="og:image" content="https://website-nextjs-nine.vercel.app/favicon.ico" key="image" />
         </Head>
-        <h2 className={styles.title}>Coming Soon!</h2>
+        
+        <h2 className={styles.title}>Coming soon!</h2>
+        <p className={styles.teaser}>Until then, go look at my <Link href="/summaries"><a>book summaries</a></Link> or <Link href="/blogs"><a>blog posts</a></Link>!</p>
     </>
 }
