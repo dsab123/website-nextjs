e1ec2a23b7c84e8b0c27c3462f4549cd1227bb93
dsab123 <dsabbaghumd@gmail.com>
2020-12-09 20:46:27 -0500

    fixed typo in json

--- pages/api/quotable-lookup.ts
+++ pages/api/quotable-lookup.ts
@@ -1,6 +1,6 @@
 import { NextApiRequest, NextApiResponse } from "next";
-import quotables from '../../data/quotable.json';
+import quotable from '../../data/quotable.json';
 
 export default async function (req: NextApiRequest, res: NextApiResponse) {
-    res.status(200).json(quotables.quotables);
+    res.status(200).json(quotable.quotables);
 }
