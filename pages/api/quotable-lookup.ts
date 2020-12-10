e03a500efa5142db0e0c7ca61088ab607896f9a4
dsab123 <dsabbaghumd@gmail.com>
2020-12-09 20:53:14 -0500

    add console.table to debug build

--- pages/api/quotable-lookup.ts
+++ pages/api/quotable-lookup.ts
@@ -2,5 +2,6 @@ import { NextApiRequest, NextApiResponse } from "next";
 import quotable from '../../data/quotable.json';
 
 export default async function (req: NextApiRequest, res: NextApiResponse) {
+    console.table(quotable.quotables);
     res.status(200).json(quotable.quotables);
 }
