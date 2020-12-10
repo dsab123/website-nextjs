0201c098b9267278282e324b2e3289d10be019dd
dsab123 <dsabbaghumd@gmail.com>
2020-12-09 21:01:10 -0500

    what about with end instead of json

--- pages/api/quotable-lookup.ts
+++ pages/api/quotable-lookup.ts
@@ -2,6 +2,6 @@ import { NextApiRequest, NextApiResponse } from "next";
 import quotable from '../../data/quotable.json';
 
 export default async function (req: NextApiRequest, res: NextApiResponse) {
-    console.table(quotable.quotables);
-    res.status(200).json(quotable.quotables);
+    console.log(quotable.quotables);
+    res.status(200).end(quotable.quotables);
 }
