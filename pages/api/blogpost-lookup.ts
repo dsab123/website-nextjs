b660d554070c4b54329ace99c61c51d73cd0a9bf
dsab123 <dsabbaghumd@gmail.com>
2020-12-10 17:49:39 -0500

    remove useless type declaration

--- pages/api/blogpost-lookup.ts
+++ pages/api/blogpost-lookup.ts
@@ -1,14 +1,6 @@
 import { NextApiRequest, NextApiResponse } from "next";
 import blogpost from '../../data/blogpost.json';
 
-type BlogpostLookupItem = {
-    blogpostId: number,
-    slug: string,
-    title: string,
-    teaser: string,
-    isReady: true
-};
-
 export default async function (req: NextApiRequest, res: NextApiResponse) {
     res.status(200).json(blogpost.blogposts);
 }
