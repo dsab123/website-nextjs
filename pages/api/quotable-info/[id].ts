f637bee0303417a5d8ce46efaf22c0cbfba18ddf
dsab123 <dsabbaghumd@gmail.com>
2020-12-11 07:01:59 -0500

    more debug

--- pages/api/quotable-info/[id].ts
+++ pages/api/quotable-info/[id].ts
@@ -12,6 +12,8 @@ type Quotable = {
 }
 
 export default async function (req: NextApiRequest, res: NextApiResponse) {
+    console.log('begin')
+
     const { query: { id } } = req;
 
     const ids = ((String) (id)).split(',').map(i => parseInt(i));
@@ -19,12 +21,14 @@ export default async function (req: NextApiRequest, res: NextApiResponse) {
     const quotables = quotable.quotables.filter(x => ids.includes(x.quotableId));
     
      if (quotables.length > 0) {
+
+        console.log('returning 200 probs')
         if (quotables.length == 1) {
             return res.status(200).json(quotables.shift());    
          }
 
         return res.status(200).json(quotables);
     }
-
+console.log('returning 404')
     return res.status(404).end();
 }
