1bbf3e1717d803258d9f4b1c1894d267fc838448
dsab123 <dsabbaghumd@gmail.com>
2020-12-17 14:21:24 -0500

    add google analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,25 @@
+import { useEffect } from "react";
+import { AppProps } from "next/app";
+import { useRouter } from "next/router";
 import Head from 'next/head'
 import Header from '../components/Header';
 import Footer from '../components/Footer';
+import * as gtag from "../utils/gtag";
 import '../styles/globals.css'
 
-function App({ Component, pageProps }) {
+function App({ Component, pageProps }: AppProps) {
+  const router = useRouter();
+
+  useEffect(() => {
+    const handleRouteChange = (url: URL) => {
+      gtag.pageview(url);
+    };
+    router.events.on("routeChangeComplete", handleRouteChange);
+    return () => {
+      router.events.off("routeChangeComplete", handleRouteChange);
+    };
+  }, [router.events]);
+  
   return (
     <>
     <Head>
