f0c552dfc0fccc24281a20f4d0231eb9cbd538f0
dsab123 <dsabbaghumd@gmail.com>
2021-01-14 13:13:14 -0500

    add new post info and classes for text

--- pages/api/blogpost-contents/[slug].ts
+++ pages/api/blogpost-contents/[slug].ts
@@ -1,14 +1,14 @@
 import { NextApiRequest, NextApiResponse } from "next";
-//import fs from 'fs';
+import fs from 'fs';
 
 export default async function (req: NextApiRequest, res: NextApiResponse) {
     const { query: { slug } } = req;
 
-    const raw = await fetch(`https://7dfaiqkhk5.execute-api.us-east-1.amazonaws.com/stage/booksummary?path=/blogposts/${slug}.md`);
-    const data = await raw.json() as BlogPostContents;
-    res.json(data);
+    // const raw = await fetch(`https://7dfaiqkhk5.execute-api.us-east-1.amazonaws.com/stage/booksummary?path=/blogposts/${slug}.md`);
+    // const data = await raw.json() as BlogPostContents;
+    // res.json(data);
 
-    // const raw = fs.readFileSync(`/Users/daniel/workspace/book-summaries/blogposts/${slug}.md`, 'utf8');
-    // console.log(`env is: ${process.env.NODE_ENV}`);
-    // return res.send({data: raw});
+    const raw = fs.readFileSync(`/Users/daniel/workspace/book-summaries/blogposts/${slug}.md`, 'utf8');
+    console.log(`env is: ${process.env.NODE_ENV}`);
+    return res.send({data: raw});
 }
