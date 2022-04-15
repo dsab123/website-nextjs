c8088d64c8d84fe296511ca2cb220319c18d3c23
Daniel Sabbagh <dsabbagh@sequoiainc.com>
2022-04-15 16:12:27 -0400

    add new blog post

--- pages/api/blogpost-contents/[slug].ts
+++ pages/api/blogpost-contents/[slug].ts
@@ -4,6 +4,7 @@ import fs from 'fs';
 export default async function (req: NextApiRequest, res: NextApiResponse) {
     const { query: { slug } } = req;
 
+    // todo move this to an environment variable dum-dum
     const raw = await fetch(`https://7dfaiqkhk5.execute-api.us-east-1.amazonaws.com/stage/booksummary?path=/blogposts/${slug}.md`);
     const data = await raw.json() as BlogPostContents;
     res.json(data);
