3925c7c19deca19e493ace581e48a5936896ae10
Daniel Sabbagh <dsabbaghumd@gmail.com>
2022-04-26 08:23:16 -0400

    put new post up on front page

--- pages/index.tsx
+++ pages/index.tsx
@@ -61,7 +61,7 @@ export default function Home() {
   
 
   useEffect(() => {
-    fetchLatestBlogPostInfo(16) // hardcoded for now
+    fetchLatestBlogPostInfo(20) // hardcoded for now
       .then(info => {
         if (isFirstPostLoading) {
           setFirstPostInfo(info);
