9708819c4267995f2b9ee1b1dcb2a2440ad0c158
dsab123 <dsabbaghumd@gmail.com>
2022-06-17 14:30:14 -0400

    fixed card layout, added dates

--- components/BlogPostCard.tsx
+++ components/BlogPostCard.tsx
@@ -5,9 +5,9 @@ export default function BlogPostCard(props: { post: BlogPostInfo; isLoading: boo
     const {post, isLoading, setIsLoading} = props;
 
     return <>
-        <div key={post.blogpostId} className={styles.postCardContainer}>
-            <Link href='/blog/[id]/[slug]' as={`/blog/${post.blogpostId}/${post.slug}`}>
-                <a className={styles.postLinks} onClick={() => setIsLoading(true)}>
+        <Link href='/blog/[id]/[slug]' as={`/blog/${post.blogpostId}/${post.slug}`}>
+            <a className={styles.postLinks} onClick={() => setIsLoading(true)}>
+                <div key={post.blogpostId} className={styles.postCardContainer}>
                     <div className={styles.postCardContent}>
                         <div className={isLoading ? styles.loadingPostImage : ''}>
                             <img className={!isLoading ? styles.postCardImage : styles.hiddenImage} src={`/${post.imageUri}`}/>
@@ -18,9 +18,12 @@ export default function BlogPostCard(props: { post: BlogPostInfo; isLoading: boo
                         <div className={isLoading ? styles.loadingPostText : ''}>
                             <p className={styles.postCardTeaser}>{!isLoading && `${post.teaser}`} </p>
                         </div>
+                        <div className={isLoading ? styles.loadingPostText : ''}>
+                            <p className={styles.postCardDate}>{!isLoading && `${post.date}`} </p>
+                        </div>
                     </div>
-                </a>
-            </Link>
-        </div>
+                </div>
+            </a>
+        </Link>
     </>;
 }
