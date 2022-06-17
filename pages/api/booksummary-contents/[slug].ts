cc5cc57def4ed3962f4d4f0f11934c32d09fabaa
dsab123 <dsabbaghumd@gmail.com>
2022-06-17 15:54:19 -0400

    hide summary endpoint, use filesystem for dev

--- pages/api/booksummary-contents/[slug].ts
+++ pages/api/booksummary-contents/[slug].ts
@@ -1,10 +1,18 @@
 import { NextApiRequest, NextApiResponse } from "next";
+import fs from 'fs';
 
 export default async function (req: NextApiRequest, res: NextApiResponse) {
     const { query: { slug } } = req;
 
-    const raw = await fetch(`https://7dfaiqkhk5.execute-api.us-east-1.amazonaws.com/stage/booksummary?path=/summaries/${slug}.md`);
-    const data = await raw.json() as BookSummaryContents;
+    let raw;
 
+    if (process.env.NODE_ENV === "development") {
+        console.log('endpoint: ' + process.env.SUMMARY_CONTENTS_ENDPOINT);
+        raw = fs.readFileSync(process.env.SUMMARY_CONTENTS_ENDPOINT + `${slug}.md`, 'utf8');
+        return res.send({data: raw});
+    }
+
+    raw = await fetch(process.env.SUMMARY_CONTENTS_ENDPOINT + `${slug}.md`);
+    const data = await raw.json() as BookSummaryContents;
     res.json(data);
 }
