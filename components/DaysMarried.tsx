6fe300a891799b22058e02491e3ab4b27ba8d830
dsab123 <dsabbaghumd@gmail.com>
2023-01-06 19:00:44 -0500

    get count up working on days married

--- components/DaysMarried.tsx
+++ components/DaysMarried.tsx
@@ -1,10 +1,20 @@
-export default function DaysMarried() {
+import { useState } from 'react';
+import CountUp from 'react-countup'
+import VisibilitySensor from 'react-visibility-sensor';
 
+
+export default function DaysMarried() {
+  const [visible, setVisible] = useState(false);
   const days = Math.floor(((new Date()).getTime() - new Date('08-01-2020').getTime()) / (1000 * 60 * 60 * 24));
 
   return <>
-    <span>
-      {days}
-    </span>
+    <CountUp end={days} useEasing={true}>
+      {({ countUpRef, start }) => (
+        <VisibilitySensor onChange={start} active={!visible}>
+          <span ref={countUpRef} />
+        </VisibilitySensor>
+      )}
+    </CountUp>
   </>
 }
+
