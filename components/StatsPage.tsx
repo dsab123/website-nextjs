f5395dc7fe57e365696ce24bd39c15c2b5c74e64
Daniel Sabbagh <dsabbaghumd@gmail.com>
2026-03-16 09:29:54 -0400

    just remove files

--- components/StatsPage.tsx
+++
@@ -1,9 +0,0 @@
-// // pages/stats.tsx
-// import postsJson from '../data/blogpost.json';
-// import TagForceGraph3D from '../components/TagForceGraph3d';
-
-// export default function StatsPage2() {
-//   const posts = (postsJson as any).blogposts;
-//   return <TagForceGraph3D posts={posts} />;
-// }
-
